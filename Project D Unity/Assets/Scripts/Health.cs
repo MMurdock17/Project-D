4f7832d4b320beca57606a2ff710651572fd246c
MMurdock17 <mattiemurdock1@gmail.com>
2025-12-07 01:52:01 -0500

    getting weapon set up

--- Project D Unity/Assets/Scripts/Health.cs
+++ Project D Unity/Assets/Scripts/Health.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int health;
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
