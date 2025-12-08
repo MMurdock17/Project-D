ecf79d4c5cebfa3e0ce03968a0fdba29bfd7cc1e
MMurdock17 <mattiemurdock1@gmail.com>
2025-12-07 19:46:10 -0500

    getting assets imported

--- Project D Unity/Assets/Scripts/Weapon.cs
+++ Project D Unity/Assets/Scripts/Weapon.cs
@@ -1,170 +1,47 @@
 using UnityEngine;
-using TMPro;
 
 public class Weapon : MonoBehaviour
 {
-    public int damage;
+    public int damage = 10;
     public Camera camera;
-    public float fireRate;
-
-    [Header("VFX")]
+    public float fireRate = 5f; 
 
     public GameObject hitVFX;
 
     private float nextFire;
 
-    [Header("Ammo")]
-
-    public int mag = 5;
-    public int ammo = 30;
-    public int magAmmo = 30;
-
-    [Header("SFX")]
-    public int shootSFXIndex = 0;
-
-    [Header("UI")]
-    
-    public TextMeshProUGUI magText;
-    public TextMeshProUGUI ammoText;
-
-    [Header("Animation")]
-
-    public Animation anim;
-    public AnimationClip reload;
-
-    [Header("Recoil Settings")]
-
-    //[Range(0, 1)]
-    //public float recoilPercent = 0.3f;
-
-    [Range(0, 2)]
-    public float recoverPercent = 0.7f;
-
-    public float recoilUp = 0.07f;
-
-    public float recoilBack = 0.07f;
-
-    private Vector3 originalPosition;
-    private Vector3 recoilVelocity = Vector3.zero;
-
-    private bool recoiling;
-    public bool recovering;
-
-    private float recoilLength;
-    private float recoverLength;
-
-    void Start()
-    {
-        magText.text = mag.ToString();
-        ammoText.text = ammo + "/" + magAmmo;
-
-        originalPosition = transform.localPosition;
-
-        recoilLength = 0;
-        recoverLength = 1 / fireRate * recoverPercent;
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
         if (nextFire > 0)
-        {
             nextFire -= Time.deltaTime;
-        }
 
-        if (Input.GetButton("Fire1") && nextFire <= 0 && ammo > 0 && GetComponent<Animation>().isPlaying == false)
+        if (Input.GetButton("Fire1") && nextFire <= 0)
         {
-            nextFire = 1 / fireRate;
-            ammo--;
-
-            magText.text = mag.ToString();
-            ammoText.text = ammo + "/" + magAmmo;
-
+            nextFire = 1f / fireRate;
             Fire();
         }
-        if (Input.GetKeyDown(KeyCode.R) && mag > 0)
-        {
-            Reload();
-        }
-
-        if (recoiling)
-        {
-            Recoil();
-        }
-
-        if (recovering)
-        {
-            Recover();
-        }
-
-    }
-
-    void Reload()
-    {
-        GetComponent<Animation>().Play(reload.name);
-
-        if (mag > 0)
-        {
-            mag--;
-            ammo = magAmmo;
-        }
-
-        magText.text = mag.ToString();
-        ammoText.text = ammo + "/" + magAmmo;
-
     }
 
     void Fire()
     {
-
-        recoiling = true;
-        recovering = false;
-
         Ray ray = new Ray(camera.transform.position, camera.transform.forward);
-
         RaycastHit hit;
 
-        if (Physics.Raycast(ray.origin, ray.direction, out hit, 100f))
+        
+        if (Physics.Raycast(ray, out hit, 100f))
         {
-            return;
+            
+            Debug.Log("Hit object: " + hit.transform.name);
 
-            if (hit.transform.gameObject.GetComponent<Health>())
+            
+            if (hitVFX != null)
             {
-                if (damage >= hit.transform.gameObject.GetComponent<Health>().health)
-                {
-
-    
-                }
-
+                Instantiate(hitVFX, hit.point, Quaternion.LookRotation(hit.normal));
             }
         }
-    }
-
-    void Recoil()
-    {
-        Vector3 finalPosition = new Vector3(originalPosition.x, originalPosition.y + recoilUp, originalPosition.z - recoilBack);
-
-        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoilLength);
-
-        if (transform.localPosition == finalPosition)
+        else
         {
-            recoiling = false;
-            recovering = true;
+            Debug.Log("Weapon fired but hit nothing.");
         }
     }
-
-    void Recover()
-    {
-        Vector3 finalPosition = originalPosition;
-
-        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, finalPosition, ref recoilVelocity, recoverLength);
-
-        if (transform.localPosition == finalPosition)
-        {
-            recoiling = false;
-            recovering = false;
-        }
-    }
-
 }
